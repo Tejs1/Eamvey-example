497492bd74c264118fb567b6ca44d154e23ed21a
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-12-23 22:10:54 +0530

    Created Select and subSelect dropdown related to 1st Select

--- Dynamically_Populate_html - main/app.js
+++ Dynamically_Populate_html - main/app.js
@@ -44,6 +44,48 @@ const createForm = (e) => {
   submitBtn.setAttribute("type", "button");
   submitBtn.innerHTML = "Submit";
 
+  const labelMajor = document.createElement("label");
+  labelMajor.setAttribute("for", "major");
+  labelMajor.innerHTML = "Major: ";
+
+  const major = document.createElement("select");
+  major.setAttribute("name", "major");
+  major.setAttribute("class", "major");
+
+  const option1 = document.createElement("option");
+  option1.setAttribute("value", "CSE");
+  option1.innerHTML = "CSE";
+  const option2 = document.createElement("option");
+  option2.setAttribute("value", "ECE");
+  option2.innerHTML = "ECE";
+  const option3 = document.createElement("option");
+  option3.setAttribute("value", "EEE");
+  option3.innerHTML = "EEE";
+
+  major.appendChild(option1);
+  major.appendChild(option2);
+  major.appendChild(option3);
+
+  const labelDomain = document.createElement("label");
+  labelDomain.setAttribute("for", "domain");
+  labelDomain.innerHTML = "Domain: ";
+
+  const domain = document.createElement("select");
+  domain.setAttribute("name", "Domain");
+  domain.setAttribute("class", "domain");
+
+  let subOption1 = document.createElement("option");
+  subOption1.setAttribute("value", "AI");
+  subOption1.setAttribute("class", "subOption1");
+  subOption1.innerHTML = "AI";
+  let subOption2 = document.createElement("option");
+  subOption2.setAttribute("value", "ML");
+  subOption2.setAttribute("class", "subOption2");
+  subOption2.innerHTML = "ML";
+
+  domain.appendChild(subOption1);
+  domain.appendChild(subOption2);
+
   form.appendChild(label);
   form.appendChild(input);
   // Inserting a line break
@@ -56,11 +98,49 @@ const createForm = (e) => {
   form.appendChild(inputMail);
 
   form.appendChild(br.cloneNode());
+  form.appendChild(labelMajor);
+  form.appendChild(major);
+
+  form.appendChild(br.cloneNode());
+  form.appendChild(labelDomain);
+  form.appendChild(domain);
+
+  form.appendChild(br.cloneNode());
   form.appendChild(submitBtn);
 
   output.appendChild(form);
 
+  major.addEventListener("input", selectHandler);
+
   btn.setAttribute("disabled", "true");
+};
+
+const selectHandler = (e) => {
+  let majorSelected = e.target.value;
+  console.log(majorSelected);
+  const sO1 = document.querySelector(".subOption1");
+  const sO2 = document.querySelector(".subOption2");
+
+  switch (majorSelected) {
+    case "CSE":
+      sO1.setAttribute("value", "AI");
+      sO1.innerHTML = "AI";
+      sO2.setAttribute("value", "ML");
+      sO2.innerHTML = "ML";
+      break;
+    case "ECE":
+      sO1.setAttribute("value", "ECE1");
+      sO1.innerHTML = "ECE1";
+      sO2.setAttribute("value", "ECE2");
+      sO2.innerHTML = "ECE2";
+      break;
+    case "EEE":
+      sO1.setAttribute("value", "EEE1");
+      sO1.innerHTML = "EEE1";
+      sO2.setAttribute("value", "EEE2");
+      sO2.innerHTML = "EEE2";
+      break;
+  }
 };
 
 btn.addEventListener("click", createForm);
